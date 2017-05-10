6148f51564429c428ef2552c77793cf167530fc6
Sanjay Patra <sanjay.patra@icann.org>
2017-05-10 17:51:56 +0530

    Test cases has modified

--- src/test/java/com/zensar/AppTest.java
+++ src/test/java/com/zensar/AppTest.java
@@ -6,34 +6,39 @@ import junit.framework.TestSuite;
 import org.junit.Ignore;
 
 /**
- * Unit test for simple App.
- */
+ * Unit test for simple App.*/
+
+
+@Ignore
 public class AppTest
     extends TestCase
 {
-    /**
+/**
      * Create the test case
      *
      * @param testName name of the test case
      */
+
 
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+/**
+ *//*    * @return the suite of tests being tested*/
+
+
     public static Test suite()
     {
         return new TestSuite(
                 AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+/**
+     * Rigourous Test :-)*/
+
+
     public void testApp()
     {
         assertTrue( true );
